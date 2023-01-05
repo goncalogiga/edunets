9db9848778a75de6531acb4433edd1f88a2e2e93
goncalogiga <principal.maiagiga@gmail.com>
2023-01-05 19:32:46 +0100

    typo

--- edunets/functions.py
+++ edunets/functions.py
@@ -4,6 +4,7 @@ from edunets.tensor import Tensor
 
 def argmax(t, **kargs): return Tensor(np.argmax(t.data, **kargs))
 
+
 def one_hot(t): 
     b = np.zeros((t.data.size, int(t.data.max() + 1)), dtype=int)
     b[np.arange(t.data.size), t.data.astype(int)] = 1.0
