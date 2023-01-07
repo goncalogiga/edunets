14ee6c798205446c775956c4679076096a068f57
goncalogiga <principal.maiagiga@gmail.com>
2023-01-07 13:38:55 +0100

    Added a pytorch example so the similarities with the edunets version are more clear

--- edunets/functions.py
+++ edunets/functions.py
@@ -2,10 +2,5 @@ import numpy as np
 from edunets.tensor import Tensor
 
 
-def argmax(t, **kargs): return Tensor(np.argmax(t.data, **kargs))
-
-
-def one_hot(t): 
-    b = np.zeros((t.data.size, int(t.data.max() + 1)), dtype=int)
-    b[np.arange(t.data.size), t.data.astype(int)] = 1.0
-    return Tensor(b)
+def argmax(t, **kargs): 
+    return Tensor(np.argmax(t.data, **kargs))
