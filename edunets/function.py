fixed several bugs in expand, reshape, repeat functions; still have to figure out the right conv formula for the conv2d implementation.

edunets/function.py
@@ -117,7 +117,7 @@ class Function:
             try:
                 brodcast_shape = np.broadcast(a.data, b.data).shape
             except ValueError:
-                raise ValueError(f"Shape of tensors mismatch: {a.shape} x {b.shape}.")
+                raise ValueError(f"Shape of tensors mismatch: {a.shape} x {b.shape}. (in operation '{self.op}')")
         
             a_shape, b_shape = a.shape, b.shape
             

edunets/functional.py
@@ -33,11 +33,11 @@ def conv(input: Tensor, kernel: Tensor, stride: int=1, padding_mode: str="consta
     + dilation: int|tuple (default: 1)
         Spacing between kernel elements. 
     """
-    chosen_padding = lambda x, pad_width: x.pad(pad_width, mode=padding_mode)
-
     # === padding ===
      # padding='valid' is the same as no padding
     if padding != 0 and padding != "valid":
+        chosen_padding = lambda x, pad_width: x.pad(pad_width, mode=padding_mode)
+
         # ~~ Squeezing input before padding ~~
         # We remove the non-empty dimensions and replace them with -1,
         # this is so we can update the shapes after the padding
@@ -50,7 +50,13 @@ def conv(input: Tensor, kernel: Tensor, stride: int=1, padding_mode: str="consta
             input = chosen_padding(input, padding)
         elif isinstance(padding, str):
             if padding == "same":
-                ... # TODO
+                # Padding 'same' means we want the result of the convolution to
+                # be the same size of the input. The shape of the result is given by:
+                # Y = I + K - 1, where I is the shape of the input and K of the kernel
+                # We pad I so Y = (I + P) + K - 1, where P is the padding
+                # Since we cant Y = I we get P = K - 1
+                # We add an extra -1 because of the pad function implementation.
+                input = chosen_padding(input, tuple(np.array(kernel.squeeze().shape) - 2))
             else:
                 raise ValueError(f"Unknown padding mode '{padding}'. Available padding modes: 'valid', 'same'.")
         else:

edunets/ops.py
@@ -96,6 +96,7 @@ class expand(Function):
     op: str = "expand"
 
     def __init__(self, a, sizes):
+        assert isinstance(sizes, tuple), f"Expected tuple for sizes, got {type(sizes)} instead."
         self.sizes = sizes
         self.a = self.__prepare__(a)
         super().__init__(self.a)
@@ -105,31 +106,16 @@ class expand(Function):
 
     def backward(self) -> None:
         def _backward(x) -> np.ndarray:
+            # Collapse extra dimensions
             for _ in range(len(self.sizes) - len(self.a.shape)):
                 x = x.sum(axis=-1)
-            return x
+            # Sum on remaining dimensions
+            for axis, (new_axis_dim, axis_dim) in enumerate(zip(self.a.shape, x.shape)):
+                if new_axis_dim != axis_dim:
+                    x = x.sum(axis=axis)
+            return x.reshape(self.a.shape)
+
         self.a._update_grad(_backward(self.out.grad))
-
-
-class repeat(Function):
-    """
-    + Repeat inside tensor
-    """
-    op: str = "repeat"
-
-    def __init__(self, a, sizes):
-        self.sizes = sizes
-        self.a = self.__prepare__(a)
-        super().__init__(self.a)
-
-    def forward(self) -> np.ndarray:
-        x = self.a.data
-        for i, rep in self.sizes:
-            x = x.repeat(rep, axis=i)
-        return x
-
-    def backward(self) -> None:
-        self.a._update_grad()        
 
 
 class reshape(Function):
@@ -148,6 +134,21 @@ class reshape(Function):
 
     def backward(self) -> None:
         self.a._update_grad(self.out.grad.reshape(self.a.shape))
+
+
+class tile(Function):
+    op: str = "tile"
+
+    def __init__(self, a, reps):
+        self.reps = reps
+        self.a = self.__prepare__(a)
+        super().__init__(self.a)
+
+    def forward(self) -> np.ndarray:
+        return np.tile(self.a.data, self.reps)
+
+    def backward(self) -> None:
+        self.a._update_grad((np.prod(self.reps) * self.out.grad)[tuple(slice(i) for i in self.a.shape)])
 
 
 class pad(Function):